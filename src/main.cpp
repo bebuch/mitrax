527643168b7fe29411b3b3d35b2c453b7596e40a
Benjamin Buch <benni.buch@gmail.com>
2015-11-03 22:53:25 +0100

    tests with boost unit test framework

--- src/main.cpp
+++ src/main.cpp
@@ -6,64 +6,10 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#include <mitrax/raw_matrix.hpp>
+#define BOOST_TEST_MODULE mitrax test suite
+#define BOOST_TEST_NO_MAIN
+#include <boost/test/unit_test.hpp>
 
-#include <boost/type_index.hpp>
-
-#include <iostream>
-
-
-int main()try{
-	using boost::typeindex::type_id_runtime;
-	using namespace mitrax;
-
-	constexpr auto m3x3 = to_matrix< int, 3, 3 >({
-		{0, 1, 2},
-		{3, 4, 5},
-		{6, 7, 8}
-	});
-	(void)m3x3;
-	std::cout << type_id_runtime(m3x3).pretty_name() << std::endl;
-
-	constexpr auto ms3x3 = to_square_matrix< int, 3 >({
-		{0, 1, 2},
-		{3, 4, 5},
-		{6, 7, 8}
-	});
-	(void)ms3x3;
-	std::cout << type_id_runtime(ms3x3).pretty_name() << std::endl;
-
-	constexpr auto rv3 = to_row_vector< int, 3 >({0, 1, 2});
-	(void)rv3;
-	std::cout << type_id_runtime(rv3).pretty_name() << std::endl;
-
-	constexpr auto cv3 = to_col_vector< int, 3 >({0, 1, 2});
-	(void)cv3;
-	std::cout << type_id_runtime(cv3).pretty_name() << std::endl;
-
-	constexpr auto v = to_square_matrix< int, 1 >({{0}});
-	(void)v;
-	std::cout << type_id_runtime(v).pretty_name() << std::endl;
-
-	constexpr int i = v;
-	(void)i;
-
-	auto ms0x3 = with_dynamic_cols(m3x3);
-	(void)ms0x3;
-	std::cout << type_id_runtime(ms0x3).pretty_name() << std::endl;
-
-	auto ms3x0 = with_dynamic_rows(m3x3);
-	(void)ms3x0;
-	std::cout << type_id_runtime(ms3x0).pretty_name() << std::endl;
-
-	auto ms0x0 = with_dynamic_dims(m3x3);
-	(void)ms0x0;
-	std::cout << type_id_runtime(ms0x0).pretty_name() << std::endl;
-}catch(std::exception const& e){
-	std::cerr
-		<< "Exit with exception: ["
-		<< boost::typeindex::type_id_runtime(e).pretty_name() << "] "
-		<< e.what() << std::endl;
-}catch(...){
-	std::cerr << "Exit with unknown exception" << std::endl;
+int main(int argc, char* argv[], char* envp[]){
+	return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
 }
---
+++ src/raw_matrix.cpp
@@ -0,0 +1,151 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Benjamin Buch
+//
+// https://github.com/bebuch/mitrax
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#include <boost/test/unit_test.hpp>
+
+#include <mitrax/raw_matrix.hpp>
+
+
+using boost::typeindex::type_id;
+using boost::typeindex::type_id_runtime;
+using namespace mitrax;
+
+
+BOOST_AUTO_TEST_CASE(test_raw_square_matrix){
+	constexpr auto m1 = to_matrix< int, 3, 3 >({
+		{0, 1, 2},
+		{3, 4, 5},
+		{6, 7, 8}
+	});
+
+	constexpr auto m2 = to_square_matrix< int, 3 >({
+		{0, 1, 2},
+		{3, 4, 5},
+		{6, 7, 8}
+	});
+
+	BOOST_TEST(type_id_runtime(m1) == type_id_runtime(m2));
+
+	auto type1 = type_id< raw_matrix< int, 3, 3 > >();
+	BOOST_TEST(type_id_runtime(m1) == type1);
+
+	auto type2 = type_id< matrix< raw_matrix_impl< int, 3, 3 >, 3, 3 > >();
+	BOOST_TEST(type_id_runtime(m1) == type2);
+
+// 	BOOST_TEST(m1 == m2);
+
+	BOOST_TEST(m1(0, 0) == 0);
+	BOOST_TEST(m1(1, 0) == 1);
+	BOOST_TEST(m1(2, 0) == 2);
+	BOOST_TEST(m1(0, 1) == 3);
+	BOOST_TEST(m1(1, 1) == 4);
+	BOOST_TEST(m1(2, 1) == 5);
+	BOOST_TEST(m1(0, 2) == 6);
+	BOOST_TEST(m1(1, 2) == 7);
+	BOOST_TEST(m1(2, 2) == 8);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_raw_col_vector){
+	constexpr auto m = to_matrix< int, 3, 1 >({ {0, 1, 2} });
+
+	constexpr auto v = to_col_vector< int, 3 >({0, 1, 2});
+
+	BOOST_TEST(type_id_runtime(m) == type_id_runtime(v));
+
+	auto type1 = type_id< raw_matrix< int, 3, 1 > >();
+	BOOST_TEST(type_id_runtime(m) == type1);
+
+	auto type2 = type_id< raw_col_vector< int, 3 > >();
+	BOOST_TEST(type_id_runtime(m) == type2);
+
+// 	BOOST_TEST(m == v);
+
+	BOOST_TEST(m(0, 0) == 0);
+	BOOST_TEST(m(1, 0) == 1);
+	BOOST_TEST(m(2, 0) == 2);
+
+	BOOST_TEST(m[0] == 0);
+	BOOST_TEST(m[1] == 1);
+	BOOST_TEST(m[2] == 2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_raw_row_vector){
+	constexpr auto m = to_matrix< int, 1, 3 >({ {0}, {1}, {2} });
+
+	constexpr auto v = to_row_vector< int, 3 >({0, 1, 2});
+
+	BOOST_TEST(type_id_runtime(m) == type_id_runtime(v));
+
+	auto type1 = type_id< raw_matrix< int, 1, 3 > >();
+	BOOST_TEST(type_id_runtime(m) == type1);
+
+	auto type2 = type_id< raw_row_vector< int, 3 > >();
+	BOOST_TEST(type_id_runtime(m) == type2);
+
+// 	BOOST_TEST(m == v);
+
+	BOOST_TEST(m(0, 0) == 0);
+	BOOST_TEST(m(0, 1) == 1);
+	BOOST_TEST(m(0, 2) == 2);
+
+	BOOST_TEST(m[0] == 0);
+	BOOST_TEST(m[1] == 1);
+	BOOST_TEST(m[2] == 2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_one_element_matrix){
+	constexpr auto m1 = to_matrix< int, 1, 1 >({ {0} });
+	constexpr auto m2 = to_square_matrix< int, 1 >({ {0} });
+
+	constexpr auto cv = to_col_vector< int, 1 >({0});
+	constexpr auto rv = to_row_vector< int, 1 >({0});
+
+	int value1 = m1; (void)value1;
+	int value2 = m2; (void)value2;
+	int value3 = cv; (void)value3;
+	int value4 = rv; (void)value4;
+
+	BOOST_TEST(type_id_runtime(m1) == type_id_runtime(m1));
+
+	BOOST_TEST(type_id_runtime(m1) == type_id_runtime(cv));
+
+	BOOST_TEST(type_id_runtime(m1) == type_id_runtime(rv));
+
+	auto type1 = type_id< raw_matrix< int, 1, 1 > >();
+	BOOST_TEST(type_id_runtime(m1) == type1);
+
+	auto type2 = type_id< raw_row_vector< int, 1 > >();
+	BOOST_TEST(type_id_runtime(m1) == type2);
+
+	auto type3 = type_id< raw_col_vector< int, 1 > >();
+	BOOST_TEST(type_id_runtime(m1) == type3);
+
+	auto type4 = type_id< raw_square_matrix< int, 1 > >();
+	BOOST_TEST(type_id_runtime(m1) == type4);
+
+// 	BOOST_TEST(m1 == m1);
+// 	BOOST_TEST(m1 == cv);
+// 	BOOST_TEST(m1 == rv);
+
+	BOOST_TEST(m1(0, 0) == 0);
+
+	BOOST_TEST(m1[0] == 0);
+}
+
+
+// 	auto ms0x3 = with_dynamic_cols(m3x3);
+// 	std::cout << type_id_runtime(ms0x3).pretty_name() << std::endl;
+// 
+// 	auto ms3x0 = with_dynamic_rows(m3x3);
+// 	std::cout << type_id_runtime(ms3x0).pretty_name() << std::endl;
+// 
+// 	auto ms0x0 = with_dynamic_dims(m3x3);
+// 	std::cout << type_id_runtime(ms0x0).pretty_name() << std::endl;
