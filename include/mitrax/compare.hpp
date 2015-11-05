38e7d2008bda7b74464a9740ffd3045ffe596e0c
Benjamin Buch <benni.buch@gmail.com>
2015-11-05 21:35:52 +0100

    add compare support for dynamic matrices

--- include/mitrax/compare.hpp
+++ include/mitrax/compare.hpp
@@ -11,28 +11,46 @@
 
 #include "matrix.hpp"
 
+#include <stdexcept>
+
 
 namespace mitrax{
 
 
-	template < typename M1, typename M2, std::size_t Cols, std::size_t Rows >
-	constexpr bool operator==(
-		matrix< M1, Cols, Rows > const& m1,
-		matrix< M2, Cols, Rows > const& m2
+	template <
+		typename M1,
+		typename M2,
+		std::size_t C1,
+		std::size_t R1,
+		std::size_t C2,
+		std::size_t R2
+	> constexpr bool operator==(
+		matrix< M1, C1, R1 > const& m1,
+		matrix< M2, C2, R2 > const& m2
 	){
-		static_assert(Cols > 0 && Rows > 0, "Need static dimensioned matrix");
-		for(std::size_t y = 0; y < Rows; ++y){
-			for(std::size_t x = 0; x < Cols; ++x){
+		if(m1.cols() != m2.cols() || m1.rows() != m2.rows()){
+			throw std::logic_error(
+				"matrix dimensions not compatible while comparing"
+			);
+		}
+
+		// Compiler may opimize with the compile time dimension
+		std::size_t cols = C1 == 0 ? m2.cols() : m1.cols();
+		std::size_t rows = R1 == 0 ? m2.rows() : m1.rows();
+
+		for(std::size_t y = 0; y < rows; ++y){
+			for(std::size_t x = 0; x < cols; ++x){
 				if(m1(x, y) != m2(x, y)) return false;
 			}
 		}
+
 		return true;
 	}
 
-	template < typename M1, typename M2, std::size_t Cols, std::size_t Rows >
+	template < typename M1, typename M2, std::size_t C, std::size_t R >
 	constexpr bool operator!=(
-		matrix< M1, Cols, Rows > const& m1,
-		matrix< M2, Cols, Rows > const& m2
+		matrix< M1, C, R > const& m1,
+		matrix< M2, C, R > const& m2
 	){
 		return !(m1 == m2);
 	}
