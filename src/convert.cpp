a9ad292c6f38476a7ef07d45eabafb471b129766
Benjamin Buch <benni.buch@gmail.com>
2015-11-08 20:09:24 +0100

    add more unit tests for conversions

--- src/convert.cpp
+++ src/convert.cpp
@@ -9,7 +9,6 @@
 #include <boost/test/unit_test.hpp>
 
 #include <mitrax/raw_matrix.hpp>
-#include <mitrax/compare.hpp>
 
 
 using boost::typeindex::type_id;
@@ -26,35 +25,297 @@ namespace{
 	});
 
 	constexpr auto ref_double = to_square_matrix< double, 2 >({
-		{0, 1},
-		{2, 3}
-	});
-
-	constexpr auto ref_double_half = to_square_matrix< double, 2 >({
 		{0.5, 1.3},
 		{2.4, 3.8}
 	});
+
+
+	template < typename M >
+	constexpr bool check(M const& m){
+		return
+			m.cols() == 2 &&
+			m.rows() == 2 &&
+			m(0, 0) == 0 &&
+			m(1, 0) == 1 &&
+			m(0, 1) == 2 &&
+			m(1, 1) == 3;
+	}
 
 
 }
 
 
 BOOST_AUTO_TEST_CASE(test_convert_2x2_to_2x2){
-	constexpr auto m = ref_double_half.as_raw_matrix< int, 2, 2 >();
+	constexpr auto m = ref_double.as_raw_matrix< int, 2, 2 >();
 
-	constexpr auto eq1 = m == ref_int;
+	constexpr auto eq1 = check(m);
 	BOOST_TEST(eq1);
 
-	constexpr auto neq1 = m != ref_double_half;
-	BOOST_TEST(neq1);
 
-	auto double_m = ref_double_half;
+	auto double_m = ref_double;
 	auto int_m = std::move(double_m).as_raw_matrix< int, 2, 2 >();
 
-	auto eq2 = int_m == ref_int;
+	auto eq2 = check(int_m);
 	BOOST_TEST(eq2);
+}
 
-	auto neq2 = int_m != ref_double_half;
-	BOOST_TEST(neq2);
+BOOST_AUTO_TEST_CASE(test_convert_2x2_to_0x2){
+	auto m = ref_double.as_raw_matrix< int, 0, 2 >();
 
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).as_raw_matrix< int, 0, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_2x2_to_2x0){
+	auto m = ref_double.as_raw_matrix< int, 2, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).as_raw_matrix< int, 2, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_2x2_to_0x0){
+	auto m = ref_double.as_raw_matrix< int, 0, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).as_raw_matrix< int, 0, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_convert_0x2_to_2x2){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 2, 2 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 2, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_0x2_to_0x2){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 0, 2 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 0, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_0x2_to_2x0){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 2, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 2, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_0x2_to_0x0){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 0, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 2 >().
+		as_raw_matrix< int, 0, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_convert_2x0_to_2x2){
+	auto m = ref_double.
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 2, 2 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 2, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_2x0_to_0x2){
+	auto m = ref_double.
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 0, 2 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 0, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_2x0_to_2x0){
+	auto m = ref_double.
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 2, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 2, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_2x0_to_0x0){
+	auto m = ref_double.
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 0, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 2, 0 >().
+		as_raw_matrix< int, 0, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_convert_0x0_to_2x2){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 2, 2 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 2, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_0x0_to_0x2){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 0, 2 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 0, 2 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_0x0_to_2x0){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 2, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 2, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
+}
+
+BOOST_AUTO_TEST_CASE(test_convert_0x0_to_0x0){
+	auto m = ref_double.
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 0, 0 >();
+
+	auto eq1 = check(m);
+	BOOST_TEST(eq1);
+
+
+	auto double_m = ref_double;
+	auto int_m = std::move(double_m).
+		as_raw_matrix< int, 0, 0 >().
+		as_raw_matrix< int, 0, 0 >();
+
+	auto eq2 = check(int_m);
+	BOOST_TEST(eq2);
 }
