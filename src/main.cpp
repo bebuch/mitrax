530e3da5d18092e35bee9fef03ce01e38d81a098
Benjamin Buch <benni.buch@gmail.com>
2015-11-10 12:29:14 +0100

    comment out unused parameter

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,6 @@
 #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
 
-int main(int argc, char* argv[], char* envp[]){
+int main(int argc, char** argv, char** /*envp*/){
 	return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
 }
