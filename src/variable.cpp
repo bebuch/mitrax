88d219fcab06e41c11f0b62dd0a82aef1e30312b
Benjamin Buch <benni.buch@gmail.com>
2015-11-10 18:37:47 +0100

    add type variable for free variables in systems of equations

---
+++ src/variable.cpp
@@ -0,0 +1,66 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Benjamin Buch
+//
+// https://github.com/bebuch/mitrax
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#include <boost/test/unit_test.hpp>
+
+#include <mitrax/variable.hpp>
+
+
+using namespace mitrax;
+
+
+BOOST_AUTO_TEST_CASE(test_variable_free){
+	variable< int > x;
+
+	int v = x;
+	BOOST_TEST(v == 0);
+
+	bool is_set = x;
+	BOOST_TEST(!is_set);
+
+	x = 5;
+	v = x;
+	BOOST_TEST(v == 5);
+
+	is_set = x;
+	BOOST_TEST(is_set);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_variable_value){
+	variable< int > x = 3;
+
+	int v = x;
+	BOOST_TEST(v == 3);
+
+	bool is_set = x;
+	BOOST_TEST(is_set);
+
+	x = 5;
+	v = x;
+	BOOST_TEST(v == 5);
+
+	is_set = x;
+	BOOST_TEST(is_set);
+}
+
+BOOST_AUTO_TEST_CASE(test_variable_output_free){
+	variable< int > x;
+
+	std::ostringstream os;
+	os << x;
+	BOOST_TEST(os.str() == "0");
+}
+
+BOOST_AUTO_TEST_CASE(test_variable_output_value){
+	variable< int > x = 5;
+
+	std::ostringstream os;
+	os << x;
+	BOOST_TEST(os.str() == "5");
+}
