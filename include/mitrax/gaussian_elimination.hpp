ed39421b6e18c68b7ebd7c36b0bae1bcadf57292
Benjamin Buch <benni.buch@gmail.com>
2015-11-12 14:31:55 +0100

    add algorithm matrix_kernel (probably not correct implemented)

--- include/mitrax/gaussian_elimination.hpp
+++ include/mitrax/gaussian_elimination.hpp
@@ -62,6 +62,55 @@ namespace mitrax{
 	}
 
 
+	template < typename M, size_t C, size_t R >
+	constexpr auto matrix_kernel(matrix< M, C, R > m){
+		using value_type = value_type_t< M >;
+
+		if(m.cols() != m.rows()){
+			throw std::logic_error(
+				"matrix_kernel does up to now only work for square matrices"
+			);
+		}
+
+		auto result = make_col_vector(m.cols()/*.as_row()*/, value_type(1));
+
+		auto ref = result.
+			template convert< std::reference_wrapper< value_type > >();
+
+		// Compiler may optimize with the compile time dimension
+		size_t const size = C == 0 ? m.rows() : m.cols();
+
+		for(size_t i = 0; i < size; ++i){
+			if(m(i, i) == 0){
+				// swap lines
+				size_t y = i + 1;
+				for(; y < size; ++y){
+					if(m(i, y) == 0) continue;
+					swap_rows(m, i, y);
+					swap_rows(ref, i, y);
+					break;
+				}
+
+				// matrix is not invertible
+				if(y == size){
+					m(i, i) = 1;
+				}
+			}
+
+			// eliminate col in lower rows
+			for(size_t y = i + 1; y < size; ++y){
+				auto factor = m(i, y) / m(i, i);
+				m(i, y) = 0;
+				for(size_t x = i + 1; x < size; ++x){
+					m(x, y) -= factor * m(x, i);
+				}
+			}
+		}
+
+		return result;
+	}
+
+
 }
 
 
