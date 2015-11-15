43654d5149c0a0fd5e10b3a4299b983552975f1f
Benjamin Buch <benni.buch@gmail.com>
2015-11-15 23:45:21 +0100

    begin to write new unit tests for the dimensions classes

--- src/integer.cpp
+++ src/integer.cpp
@@ -17,103 +17,109 @@ using namespace mitrax;
 using namespace mitrax::literals;
 
 
+namespace{
+
+
+	template < typename T >
+	auto rt_id(T&& v){
+		return type_id_runtime(static_cast< T&& >(v));
+	}
+
+	template < typename T >
+	auto const id = type_id< T >();
+
+
+}
+
+
 BOOST_AUTO_TEST_SUITE(integer)
 
 
 BOOST_AUTO_TEST_CASE(test_col_t){
-	constexpr auto s = 14_C;
-	BOOST_TEST(s.get() == size_t(14));
+	constexpr auto ct = col_t< 7 >();
+	constexpr auto rt = col_t< 0 >(7);
 
-	auto type = type_id< col_t< 14 > >();
-	BOOST_TEST(type == type_id_runtime(s.get()));
-}
+	BOOST_TEST(ct == size_t(7));
+	BOOST_TEST(rt == size_t(7));
 
-BOOST_AUTO_TEST_CASE(test_col_t0){
-	constexpr auto s = 14_C_rt;
-	BOOST_TEST(s.get() == size_t(14));
+	BOOST_TEST(rt_id(ct) == (id< col_t< 7 > >));
+	BOOST_TEST(rt_id(rt) == (id< col_t< 0 > >));
 
-	auto type = type_id< col_t< 0 > >();
-	BOOST_TEST(type == type_id_runtime(s.get()));
+	BOOST_TEST(ct.init() == size_t(7));
+	BOOST_TEST(rt.init() == size_t(7));
+
+	BOOST_TEST(rt_id(ct.init()) == (id< col_init_t< true, true, 7 > >));
+	BOOST_TEST(rt_id(rt.init()) == (id< col_init_t< false, false, 0 > >));
+
+	BOOST_TEST(ct.as_row() == size_t(7));
+	BOOST_TEST(rt.as_row() == size_t(7));
+
+	BOOST_TEST(rt_id(ct.as_row()) == (id< row_t< 7 > >));
+	BOOST_TEST(rt_id(rt.as_row()) == (id< row_t< 0 > >));
+
+	BOOST_TEST(ct.as_dim() == size_t(7));
+	BOOST_TEST(rt.as_dim() == size_t(7));
+
+	BOOST_TEST(rt_id(ct.as_dim()) == (id< dim_t< 7 > >));
+	BOOST_TEST(rt_id(rt.as_dim()) == (id< dim_t< 0 > >));
 }
 
 BOOST_AUTO_TEST_CASE(test_row_t){
-	constexpr auto s = 14_R;
-	BOOST_TEST(s.get() == size_t(14));
+	constexpr auto ct = row_t< 7 >();
+	constexpr auto rt = row_t< 0 >(7);
 
-	auto type = type_id< row_t< 14 > >();
-	BOOST_TEST(type == type_id_runtime(s.get()));
-}
+	BOOST_TEST(ct == size_t(7));
+	BOOST_TEST(rt == size_t(7));
 
-BOOST_AUTO_TEST_CASE(test_row_t0){
-	constexpr auto s = 14_R_rt;
-	BOOST_TEST(s.get() == size_t(14));
+	BOOST_TEST(rt_id(ct) == (id< row_t< 7 > >));
+	BOOST_TEST(rt_id(rt) == (id< row_t< 0 > >));
 
-	auto type = type_id< row_t< 0 > >();
-	BOOST_TEST(type == type_id_runtime(s.get()));
+	BOOST_TEST(ct.init() == size_t(7));
+	BOOST_TEST(rt.init() == size_t(7));
+
+	BOOST_TEST(rt_id(ct.init()) == (id< row_init_t< true, true, 7 > >));
+	BOOST_TEST(rt_id(rt.init()) == (id< row_init_t< false, false, 0 > >));
+
+	BOOST_TEST(ct.as_col() == size_t(7));
+	BOOST_TEST(rt.as_col() == size_t(7));
+
+	BOOST_TEST(rt_id(ct.as_col()) == (id< col_t< 7 > >));
+	BOOST_TEST(rt_id(rt.as_col()) == (id< col_t< 0 > >));
+
+	BOOST_TEST(ct.as_dim() == size_t(7));
+	BOOST_TEST(rt.as_dim() == size_t(7));
+
+	BOOST_TEST(rt_id(ct.as_dim()) == (id< dim_t< 7 > >));
+	BOOST_TEST(rt_id(rt.as_dim()) == (id< dim_t< 0 > >));
 }
 
 BOOST_AUTO_TEST_CASE(test_dim_t){
-	constexpr auto s = 14_D;
-	BOOST_TEST(s.get() == size_t(14));
-	BOOST_TEST(s.get().as_col() == size_t(14));
-	BOOST_TEST(s.as_col().get() == size_t(14));
-	BOOST_TEST(s.get().as_row() == size_t(14));
-	BOOST_TEST(s.as_row().get() == size_t(14));
+	constexpr auto ct = dim_t< 7 >();
+	constexpr auto rt = dim_t< 0 >(7);
 
-	auto type = type_id< dim_t< 14 > >();
-	BOOST_TEST(type == type_id_runtime(s.get()));
-}
+	BOOST_TEST(ct == size_t(7));
+	BOOST_TEST(rt == size_t(7));
 
-BOOST_AUTO_TEST_CASE(test_dim_t0){
-	constexpr auto s = 14_D_rt;
-	BOOST_TEST(s.get() == size_t(14));
-	BOOST_TEST(s.get().as_col() == size_t(14));
-	BOOST_TEST(s.as_col().get() == size_t(14));
-	BOOST_TEST(s.get().as_row() == size_t(14));
-	BOOST_TEST(s.as_row().get() == size_t(14));
+	BOOST_TEST(rt_id(ct) == (id< dim_t< 7 > >));
+	BOOST_TEST(rt_id(rt) == (id< dim_t< 0 > >));
 
-	auto type1 = type_id< dim_t< 0 > >();
-	BOOST_TEST(type1 == type_id_runtime(s.get()));
+	BOOST_TEST(ct.init() == size_t(7));
+	BOOST_TEST(rt.init() == size_t(7));
 
-	auto type2 = type_id< col_t< 0 > >();
-	BOOST_TEST(type2 == type_id_runtime(s.get().as_col()));
+	BOOST_TEST(rt_id(ct.init()) == (id< dim_init_t< true, true, 7 > >));
+	BOOST_TEST(rt_id(rt.init()) == (id< dim_init_t< false, false, 0 > >));
 
-	auto type3 = type_id< row_t< 0 > >();
-	BOOST_TEST(type3 == type_id_runtime(s.get().as_row()));
-}
+	BOOST_TEST(ct.as_col() == size_t(7));
+	BOOST_TEST(rt.as_col() == size_t(7));
 
-BOOST_AUTO_TEST_CASE(test_int_to_size_t){
-	BOOST_TEST(col_t< 14 >() == size_t(14));
-	BOOST_TEST(col_t< 0 >(14) == size_t(14));
-	BOOST_TEST((col_init_t< true, true, 14 >()) == size_t(14));
-	BOOST_TEST((col_init_t< true, false, 14 >()) == size_t(14));
-	BOOST_TEST((col_init_t< true, true, 14 >()).get() == size_t(14));
-	BOOST_TEST((col_init_t< true, false, 14 >()).get() == size_t(14));
+	BOOST_TEST(rt_id(ct.as_col()) == (id< col_t< 7 > >));
+	BOOST_TEST(rt_id(rt.as_col()) == (id< col_t< 0 > >));
 
-	BOOST_TEST(row_t< 14 >() == size_t(14));
-	BOOST_TEST(row_t< 0 >(14) == size_t(14));
-	BOOST_TEST((row_init_t< true, true, 14 >()) == size_t(14));
-	BOOST_TEST((row_init_t< true, false, 14 >()) == size_t(14));
-	BOOST_TEST((row_init_t< true, true, 14 >()).get() == size_t(14));
-	BOOST_TEST((row_init_t< true, false, 14 >()).get() == size_t(14));
+	BOOST_TEST(ct.as_row() == size_t(7));
+	BOOST_TEST(rt.as_row() == size_t(7));
 
-	BOOST_TEST(dim_t< 14 >() == size_t(14));
-	BOOST_TEST(dim_t< 0 >(14) == size_t(14));
-	BOOST_TEST((dim_init_t< true, true, 14 >()) == size_t(14));
-	BOOST_TEST((dim_init_t< true, false, 14 >()) == size_t(14));
-	BOOST_TEST((dim_init_t< true, true, 14 >()).get() == size_t(14));
-	BOOST_TEST((dim_init_t< true, false, 14 >()).get() == size_t(14));
-	BOOST_TEST((dim_init_t< true, true, 14 >()).as_col() == size_t(14));
-	BOOST_TEST((dim_init_t< true, false, 14 >()).as_col() == size_t(14));
-	BOOST_TEST((dim_init_t< true, true, 14 >()).as_row() == size_t(14));
-	BOOST_TEST((dim_init_t< true, false, 14 >()).as_row() == size_t(14));
-
-	BOOST_TEST(col_t< 14 >() == 14_C);
-	BOOST_TEST(col_t< 0 >(14) == 14_C_rt);
-
-	// TODO: Suppress this:
-	BOOST_TEST(col_t< 14 >() == 14_R);
-	BOOST_TEST(col_t< 0 >(14) == 14_R_rt);
+	BOOST_TEST(rt_id(ct.as_row()) == (id< row_t< 7 > >));
+	BOOST_TEST(rt_id(rt.as_row()) == (id< row_t< 0 > >));
 }
 
 
