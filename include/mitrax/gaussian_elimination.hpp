ffb7d723dfe4bc34bb643bf180d34cb50573e6d4
Benjamin Buch <benni.buch@gmail.com>
2015-11-26 13:32:36 +0100

    use value type in gaussian elimination

--- include/mitrax/gaussian_elimination.hpp
+++ include/mitrax/gaussian_elimination.hpp
@@ -132,6 +132,100 @@ namespace mitrax{
 	}
 
 
+	template <
+		typename M1, size_t C1, size_t R1,
+		typename M2, size_t R2,
+		typename M3, size_t R3
+	> constexpr auto gaussian_elimination(
+		matrix< M1, C1, R1 > a,
+		col_vector< M2, R2 > v,
+		col_vector< M3, R3 > defaults
+	){
+		using value_type = std::common_type_t<
+			value_type_t< M1 >, value_type_t< M2 >, value_type_t< M3 >
+		>;
+
+		if(a.rows() == v.rows() && a.cols() != v.rows() + defaults.rows()){
+			throw std::logic_error(
+				"gaussian_elimination: incompatible dimensions"
+			);
+		}
+
+		auto b = make_col_vector_by_function(
+			(v.rows() + defaults.rows()).init(),
+			[&v, &defaults](size_t i)->value_type{
+				if(i < v.rows()){
+					return v[i];
+				}else{
+					return defaults[i - v.rows()];
+				}
+			});
+
+		for(size_t i = a.rows(); i < b.rows(); ++i){
+			for(size_t y = 0; y < a.rows(); ++y){
+				b[y] -= a(y, y) * b[i];
+			}
+		}
+
+		auto m = make_matrix_by_function(
+			a.rows().as_col().init(), a.rows().init(),
+			[&a](size_t x, size_t y)->value_type{
+				return a(x, y);
+			});
+
+		// Compiler may optimize with the compile time dimension
+		size_t const size = C1 != 0 ? m.cols() : R1 != 0 ? m.rows() : b.rows();
+
+		size_t swap_count = 0;
+		for(size_t i = 0; i < size; ++i){
+			if(m(i, i) == 0){
+				// swap lines
+				size_t y = i + 1;
+				for(; y < size; ++y){
+					if(m(i, y) == 0) continue;
+					swap_rows(m, i, y);
+					swap_rows(b, i, y);
+					++swap_count;
+					break;
+				}
+
+				// matrix is not invertible
+				if(y == size){
+					throw std::logic_error(
+						"gaussian_elimination with non invertible matrix"
+					);
+				}
+			}
+
+			// eliminate col in lower rows
+			for(size_t y = i + 1; y < size; ++y){
+				auto factor = m(i, y) / m(i, i);
+				m(i, y) = 0;
+				for(size_t x = i + 1; x < size; ++x){
+					m(x, y) -= factor * m(x, i);
+				}
+				b[y] -= factor *b[i];
+			}
+		}
+
+		for(size_t i = 1; i < size; ++i){
+			auto x = size - i;
+
+			for(size_t y = 0; y < x; ++y){
+				auto factor = m(x, y) / m(x, x);
+				b[y] -= factor * b[x];
+// 				m(x, y) = 0; // TODO: remove this line
+			}
+		}
+
+		for(size_t i = 0; i < size; ++i){
+			b[i] /= m(i, i);
+		}
+
+		return b;
+	}
+
+
 	template < typename M, size_t C, size_t R >
 	constexpr raw_matrix< value_type_t< M >, C, R >
 	inverse(matrix< M, C, R > m){
