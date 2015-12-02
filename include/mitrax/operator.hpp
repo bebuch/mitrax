a227e91c8e3338abc823ca450ff6edc89e3daf4f
Benjamin Buch <benni.buch@gmail.com>
2015-12-02 12:31:37 +0100

    add a first version of cross product

--- include/mitrax/operator.hpp
+++ include/mitrax/operator.hpp
@@ -455,6 +455,54 @@ namespace mitrax{
 	}
 
 
+	template <
+		typename M1, size_t C1, size_t R1,
+		typename M2, size_t C2, size_t R2
+	> constexpr auto cross_product(
+		matrix< M1, C1, R1 > const& m1,
+		matrix< M2, C2, R2 > const& m2
+	){
+		static_assert(
+			((
+				(C1 == 1 || C1 == 0) && (R1 == 3 || R1 == 0) &&
+				(C2 == 1 || C2 == 0) && (R2 == 3 || R2 == 0)
+			) || (
+				(C1 == 3 || C1 == 0) && (R1 == 1 || R1 == 0) &&
+				(C2 == 3 || C2 == 0) && (R2 == 1 || R2 == 0)
+			)) && (
+				(C1 != 0 && C2 != 0 && C1 == C2) &&
+				(R1 != 0 && R2 != 0 && R1 == R2)
+			),
+			"Matrix dimensions not compatible"
+		);
+
+		// Compiler should skip this for compile time dimensions
+		if(m1.cols() != m2.cols() || m1.rows() != m2.rows()){
+			throw std::logic_error(
+				"matrix dimensions not compatible while comparing"
+			);
+		}
+
+		using value_type =
+			std::common_type_t< value_type_t< M1 >, value_type_t< M2 > >;
+
+		// TODO: Use the compile time size from m1 or m2, if one is ct
+		if(m1.cols() == 1){
+			return make_col_vector< value_type >(m1.rows().init(), {
+				m1[1] * m2[2] - m1[2] * m2[1],
+				m1[2] * m2[0] - m1[0] * m2[2],
+				m1[0] * m2[1] - m1[1] * m2[0]
+			});
+		}else{
+			return make_row_vector< value_type >(m1.cols().init(), {
+				m1[1] * m2[2] - m1[2] * m2[1],
+				m1[2] * m2[0] - m1[0] * m2[2],
+				m1[0] * m2[1] - m1[1] * m2[0]
+			});
+		}
+	}
+
+
 }
 
 
