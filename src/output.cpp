dc63f852bd3ef78a1003159be418506e05e27ddc
Benjamin Buch <benni.buch@gmail.com>
2016-02-21 22:14:15 +0100

    also check output of dynamic matrices

--- src/output.cpp
+++ src/output.cpp
@@ -23,45 +23,77 @@ using namespace mitrax::literals;
 BOOST_AUTO_TEST_SUITE(test_suite_output)
 
 
-// TODO: Check also for dynamic matrices
+namespace{
 
 
-BOOST_AUTO_TEST_CASE(test_output_3x3){
-	auto m = make_square_matrix< int >(3_D, {
+	int m33[3][3] = {
 		{1, 2, 3},
 		{4, 5, 6},
 		{7, 8, 9}
-	});
+	};
 
-	std::ostringstream os;
-	os << m;
+	constexpr char const* m33_ref = "[3,3]((1,2,3),(4,5,6),(7,8,9))";
 
-	BOOST_TEST(os.str() == "[3,3]((1,2,3),(4,5,6),(7,8,9))");
-}
-
-BOOST_AUTO_TEST_CASE(test_output_2x3){
-	auto m = make_matrix< int >(2_C, 3_R, {
+	int m23[3][2] = {
 		{1, 2},
 		{4, 5},
 		{7, 8}
-	});
+	};
 
-	std::ostringstream os;
-	os << m;
+	constexpr char const* m23_ref = "[2,3]((1,2),(4,5),(7,8))";
 
-	BOOST_TEST(os.str() == "[2,3]((1,2),(4,5),(7,8))");
+	int m32[2][3] = {
+		{1, 2, 3},
+		{4, 5, 6}
+	};
+
+	constexpr char const* m32_ref = "[3,2]((1,2,3),(4,5,6))";
+
+	template < typename T >
+	std::string out(T const& m){
+		std::ostringstream os;
+		os << m;
+		return os.str();
+	}
+
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test_output_3x3){
+	auto m1 = make_matrix< int >(3_C, 3_R, m33);
+	auto m2 = make_matrix< int >(3_C_rt, 3_R, m33);
+	auto m3 = make_matrix< int >(3_C, 3_R_rt, m33);
+	auto m4 = make_matrix< int >(3_C_rt, 3_R_rt, m33);
+
+	BOOST_TEST(out(m1) == m33_ref);
+	BOOST_TEST(out(m2) == m33_ref);
+	BOOST_TEST(out(m3) == m33_ref);
+	BOOST_TEST(out(m4) == m33_ref);
+}
+
+BOOST_AUTO_TEST_CASE(test_output_2x3){
+	auto m1 = make_matrix< int >(2_C, 3_R, m23);
+	auto m2 = make_matrix< int >(2_C_rt, 3_R, m23);
+	auto m3 = make_matrix< int >(2_C, 3_R_rt, m23);
+	auto m4 = make_matrix< int >(2_C_rt, 3_R_rt, m23);
+
+	BOOST_TEST(out(m1) == m23_ref);
+	BOOST_TEST(out(m2) == m23_ref);
+	BOOST_TEST(out(m3) == m23_ref);
+	BOOST_TEST(out(m4) == m23_ref);
 }
 
 BOOST_AUTO_TEST_CASE(test_output_3x2){
-	auto m = make_matrix< int >(3_C, 2_R, {
-		{1, 2, 3},
-		{4, 5, 6}
-	});
+	auto m1 = make_matrix< int >(3_C, 2_R, m32);
+	auto m2 = make_matrix< int >(3_C_rt, 2_R, m32);
+	auto m3 = make_matrix< int >(3_C, 2_R_rt, m32);
+	auto m4 = make_matrix< int >(3_C_rt, 2_R_rt, m32);
 
-	std::ostringstream os;
-	os << m;
-
-	BOOST_TEST(os.str() == "[3,2]((1,2,3),(4,5,6))");
+	BOOST_TEST(out(m1) == m32_ref);
+	BOOST_TEST(out(m2) == m32_ref);
+	BOOST_TEST(out(m3) == m32_ref);
+	BOOST_TEST(out(m4) == m32_ref);
 }
 
 
