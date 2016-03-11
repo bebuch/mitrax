2f920325fc4dfecd2d0b7f7882f8e3c5072b0b2f
Benjamin Buch <benni.buch@gmail.com>
2016-03-11 11:38:12 +0100

    make convolution more flexible

--- include/mitrax/convolution.hpp
+++ include/mitrax/convolution.hpp
@@ -18,18 +18,30 @@ namespace mitrax{
 	namespace detail{
 
 
+		struct multiply_op{
+			template < typename T, typename U >
+			constexpr auto operator()(T const& a, U const& b)const{
+				return a * b;
+			}
+		};
+
 		template <
 			typename M1, size_t C1, size_t R1,
-			typename M2, size_t C2, size_t R2
+			typename M2, size_t C2, size_t R2,
+			typename Op
 		> struct convolution_worker{
-			using value_type =
-				std::common_type_t< value_type_t< M1 >, value_type_t< M2 > >;
+			using value_type = std::decay_t< decltype(
+					std::declval< Op >()(
+						std::declval< value_type_t< M1 > >(),
+						std::declval< value_type_t< M2 > >()
+					)
+				) >;
 
 			constexpr value_type operator()(size_t x, size_t y)const{
 				auto v = value_type();
 				for(std::size_t b = 0; b < m.rows(); ++b){
 					for(std::size_t a = 0; a < m.cols(); ++a){
-						v += i(x + a, y + b) * m(a, b);
+						v += op(i(x + a, y + b), m(a, b));
 					}
 				}
 				return v;
@@ -37,6 +49,7 @@ namespace mitrax{
 
 			matrix< M1, C1, R1 > const& i;
 			matrix< M2, C2, R2 > const& m;
+			Op const& op;
 		};
 
 
@@ -45,10 +58,12 @@ namespace mitrax{
 
 	template <
 		typename M1, size_t C1, size_t R1,
-		typename M2, size_t C2, size_t R2
+		typename M2, size_t C2, size_t R2,
+		typename Op = detail::multiply_op
 	> constexpr auto convolution(
 		matrix< M1, C1, R1 > const& i,
-		matrix< M2, C2, R2 > const& m
+		matrix< M2, C2, R2 > const& m,
+		Op const& op = detail::multiply_op()
 	){
 		static_assert(
 			(C1 == 0 || C2 == 0 || C1 >= C2) &&
@@ -63,20 +78,24 @@ namespace mitrax{
 		return make_matrix_by_function(
 			cols< 1 >() + i.cols() - m.cols(),
 			rows< 1 >() + i.rows() - m.rows(),
-			detail::convolution_worker< M1, C1, R1, M2, C2, R2 >{i, m});
+			detail::convolution_worker< M1, C1, R1, M2, C2, R2, Op >{
+				i, m, op
+			});
 	}
 
 
 	template <
 		typename M1, size_t C1, size_t R1,
 		typename M2, size_t R2,
-		typename M3, size_t C3
+		typename M3, size_t C3,
+		typename Op = detail::multiply_op
 	> constexpr auto convolution(
 		matrix< M1, C1, R1 > const& image,
 		col_vector< M2, R2 > const& vc,
-		row_vector< M3, C3 > const& vr
+		row_vector< M3, C3 > const& vr,
+		Op const& op = detail::multiply_op()
 	){
-		return convolution(convolution(image, vr), vc);
+		return convolution(convolution(image, vr, op), vc, op);
 	}
 
 
