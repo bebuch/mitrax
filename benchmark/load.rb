f4006bee776c063101696687f408ebaf75605656
Benjamin Buch <benni.buch@gmail.com>
2016-08-23 17:27:49 +0200

    convert image from JPEG to PNG after download

--- benchmark/load.rb
+++ benchmark/load.rb
@@ -1,4 +1,11 @@
 require 'open-uri'
+require 'rmagick'
+
 open('image_00.jpg', 'wb') do |file|
 	file << open('https://upload.wikimedia.org/wikipedia/commons/e/ed/02.Trinidad_(59).JPG').read
+end
+
+i = Magick::Image.read('image_00.jpg').first
+i.write('image_00.png') do
+	self.format = 'PNG'
 end
