beb81ad48f45113b27a7cef4825421241178d40a
Benjamin Buch <benni.buch@gmail.com>
2016-08-23 17:26:53 +0200

    add support for more PNG types

--- include/mitrax/png.hpp
+++ include/mitrax/png.hpp
@@ -19,6 +19,69 @@
 namespace mitrax{ namespace png{
 
 
+	template < typename T, typename PNG_type >
+	struct basic_ga{
+		T g, a;
+
+		basic_ga(): g(0), a(0) {}
+
+		basic_ga(T g, T a):
+			g(g), a(a) {}
+
+		basic_ga(::png::basic_ga_pixel< PNG_type > const& v):
+			g(v.value), a(v.alpha) {}
+
+		operator ::png::basic_ga_pixel< PNG_type >()const{
+			return {g, a};
+		}
+	};
+
+	using ga8 = basic_ga< std::uint8_t, unsigned char >;
+	using ga16 = basic_ga< std::uint16_t, unsigned short >;
+
+
+	template < typename T, typename PNG_type >
+	struct basic_rgb{
+		T r, g, b;
+
+		basic_rgb(): r(0), g(0), b(0) {}
+
+		basic_rgb(T r, T g, T b):
+			r(r), g(g), b(b) {}
+
+		basic_rgb(::png::basic_rgb_pixel< PNG_type > const& v):
+			r(v.red), g(v.green), b(v.blue) {}
+
+		operator ::png::basic_rgb_pixel< PNG_type >()const{
+			return {r, g, b};
+		}
+	};
+
+	using rgb8 = basic_rgb< std::uint8_t, unsigned char >;
+	using rgb16 = basic_rgb< std::uint16_t, unsigned short >;
+
+
+	template < typename T, typename PNG_type >
+	struct basic_rgba{
+		T r, g, b, a;
+
+		basic_rgba(): r(0), g(0), b(0), a(0) {}
+
+		basic_rgba(T r, T g, T b, T a):
+			r(r), g(g), b(b), a(a) {}
+
+		basic_rgba(::png::basic_rgba_pixel< PNG_type > const& v):
+			r(v.red), g(v.green), b(v.blue), a(v.alpha) {}
+
+		operator ::png::basic_rgba_pixel< PNG_type >()const{
+			return {r, g, b, a};
+		}
+	};
+
+	using rgba8 = basic_rgba< std::uint8_t, unsigned char >;
+	using rgba16 = basic_rgba< std::uint16_t, unsigned short >;
+
+
 	namespace detail{
 
 
@@ -35,6 +98,18 @@ namespace mitrax{ namespace png{
 			{ using type = ::png::gray_pixel_16; };
 		template <> struct png_type< std::uint16_t >
 			{ using type = ::png::gray_pixel_16; };
+		template <> struct png_type< png::ga8 >
+			{ using type = ::png::ga_pixel; };
+		template <> struct png_type< png::ga16 >
+			{ using type = ::png::ga_pixel_16; };
+		template <> struct png_type< png::rgb8 >
+			{ using type = ::png::rgb_pixel; };
+		template <> struct png_type< png::rgb16 >
+			{ using type = ::png::rgb_pixel_16; };
+		template <> struct png_type< png::rgba8 >
+			{ using type = ::png::rgba_pixel; };
+		template <> struct png_type< png::rgba16 >
+			{ using type = ::png::rgba_pixel_16; };
 
 		template < typename T >
 		using png_type_t = typename png_type< T >::type;
