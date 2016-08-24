fc6884e11456c62ee2998d72614e1651f82dfb63
Benjamin Buch <benni.buch@gmail.com>
2016-08-24 16:22:19 +0200

    use all egde detection operators

--- benchmark/convolution.cpp
+++ benchmark/convolution.cpp
@@ -8,7 +8,7 @@
 //-----------------------------------------------------------------------------
 #include <mitrax/png.hpp>
 #include <mitrax/convert.hpp>
-#include <mitrax/convolution.hpp>
+#include <mitrax/convolution/edge_operators.hpp>
 
 #include <iostream>
 #include <cmath>
@@ -20,21 +20,6 @@ int main(){
 
 	auto m = make_matrix_v< std::uint8_t >(3264_C_rt, 2448_R_rt);
 	mitrax::png::load(m, "image_01.png");
-
-	constexpr auto sobel_x = make_square_matrix< int >(3_D, {
-		{1, 0, -1},
-		{2, 0, -2},
-		{1, 0, -1}
-	});
-
-	constexpr auto sobel_y = make_square_matrix< int >(3_D, {
-		{ 1,  2,  1},
-		{ 0,  0,  0},
-		{-1, -2, -1}
-	});
-
-	auto mx = convolution(m, sobel_x);
-	auto my = convolution(m, sobel_y);
 
 	auto normalize = [](auto const& m){
 		auto minmax_iters = std::minmax_element(m.begin(), m.end());
@@ -49,20 +34,51 @@ int main(){
 		});
 	};
 
-	mitrax::png::save(normalize(mx), "image_0X.png");
-	mitrax::png::save(normalize(my), "image_0Y.png");
+	{
+		auto mx = roberts_cross_x(m);
+		auto my = roberts_cross_y(m);
+		auto ma = gradient_magnitude(mx, my);
+		auto md = gradient_direction(mx, my);
 
-	auto ma = make_matrix_fn(get_dims(mx, my), [&mx, &my](auto j, auto k){
-		auto x = mx(j, k);
-		auto y = my(j, k);
-		return std::sqrt(x * x + y * y);
-	});
+		mitrax::png::save(normalize(mx), "image_X0.png");
+		mitrax::png::save(normalize(my), "image_Y0.png");
+		mitrax::png::save(normalize(ma), "image_A0.png");
+		mitrax::png::save(normalize(md), "image_D0.png");
+	}
 
-	mitrax::png::save(normalize(ma), "image_0A.png");
+	{
+		auto mx = prewitt_x(m);
+		auto my = prewitt_y(m);
+		auto ma = gradient_magnitude(mx, my);
+		auto md = gradient_direction(mx, my);
 
-	auto md = make_matrix_fn(get_dims(mx, my), [&mx, &my](auto j, auto k){
-		return std::atan2(mx(j, k), my(j, k));
-	});
+		mitrax::png::save(normalize(mx), "image_X1.png");
+		mitrax::png::save(normalize(my), "image_Y1.png");
+		mitrax::png::save(normalize(ma), "image_A1.png");
+		mitrax::png::save(normalize(md), "image_D1.png");
+	}
 
-	mitrax::png::save(normalize(md), "image_0D.png");
+	{
+		auto mx = sobel_x(m);
+		auto my = sobel_y(m);
+		auto ma = gradient_magnitude(mx, my);
+		auto md = gradient_direction(mx, my);
+
+		mitrax::png::save(normalize(mx), "image_X2.png");
+		mitrax::png::save(normalize(my), "image_Y2.png");
+		mitrax::png::save(normalize(ma), "image_A2.png");
+		mitrax::png::save(normalize(md), "image_D2.png");
+	}
+
+	{
+		auto mx = scharr_x(m);
+		auto my = scharr_y(m);
+		auto ma = gradient_magnitude(mx, my);
+		auto md = gradient_direction(mx, my);
+
+		mitrax::png::save(normalize(mx), "image_X3.png");
+		mitrax::png::save(normalize(my), "image_Y3.png");
+		mitrax::png::save(normalize(ma), "image_A3.png");
+		mitrax::png::save(normalize(md), "image_D3.png");
+	}
 }
