0735c1531d7484299735dbd41fa226dcc48ba60f
Benjamin Buch <benni.buch@gmail.com>
2016-09-05 16:26:57 +0200

    reenable unit test make_raw_matrix

--- test/make_raw_matrix.cpp
+++ test/make_raw_matrix.cpp
@@ -6,6 +6,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
+#define BOOST_TEST_MODULE mitrax make_raw_matrix
 #include <boost/test/unit_test.hpp>
 
 #include <mitrax/make_matrix.hpp>
