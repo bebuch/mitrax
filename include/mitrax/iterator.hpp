34bad267dbd073618a62e2add080ae4da346479b
Benjamin Buch <benni.buch@gmail.com>
2016-09-14 14:17:04 +0200

    add end()-functions

--- include/mitrax/iterator.hpp
+++ include/mitrax/iterator.hpp
@@ -6,8 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#ifndef _mitrax__move_iterator__hpp_INCLUDED_
-#define _mitrax__move_iterator__hpp_INCLUDED_
+#ifndef _mitrax__iterator__hpp_INCLUDED_
+#define _mitrax__iterator__hpp_INCLUDED_
 
 #include <iterator>
 
@@ -461,6 +461,27 @@ namespace mitrax{
 	template < typename C >
 	constexpr auto cbegin(C const& c){
 		return mitrax::begin(c);
+	}
+
+
+	template < typename C >
+	constexpr auto end(C& c){
+		return c.end();
+	}
+
+	template < typename C >
+	constexpr auto end(C const& c){
+		return c.end();
+	}
+
+	template < typename T, size_t N >
+	constexpr T* end(T(&array)[N])noexcept{
+		return array + N;
+	}
+
+	template < typename C >
+	constexpr auto cend(C const& c){
+		return mitrax::end(c);
 	}
 
 
