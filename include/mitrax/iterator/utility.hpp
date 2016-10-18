3a9dc7a227c8a53c7ac2c6573f081f241e8031b3
Benjamin Buch <benni.buch@gmail.com>
2016-10-18 16:35:17 +0200

    disable mitrax::begin and end for non-container types

--- include/mitrax/iterator/utility.hpp
+++ include/mitrax/iterator/utility.hpp
@@ -10,17 +10,33 @@
 #define _mitrax__iterator__utility__hpp_INCLUDED_
 
 #include "move.hpp"
+#include "../detail/concepts.hpp"
+
+
+namespace mitrax::detail{
+
+
+	template < typename C >
+	using has_begin = decltype(std::declval< C >().begin());
+
+	template < typename C >
+	using has_end = decltype(std::declval< C >().end());
+
+
+}
 
 
 namespace mitrax{
 
 
-	template < typename C >
+	template < typename C,
+		enable_if_t< (bool)compiles< C&, detail::has_begin >() > = 0 >
 	constexpr auto begin(C& c)noexcept(noexcept(c.begin())){
 		return c.begin();
 	}
 
-	template < typename C >
+	template < typename C,
+		enable_if_t< (bool)compiles< C const&, detail::has_begin >() > = 0 >
 	constexpr auto begin(C const& c)noexcept(noexcept(c.begin())){
 		return c.begin();
 	}
@@ -35,18 +51,15 @@ namespace mitrax{
 		return make_move_iterator(array);
 	}
 
-	template < typename C >
-	constexpr auto cbegin(C const& c)noexcept(noexcept(mitrax::begin(c))){
-		return mitrax::begin(c);
-	}
 
-
-	template < typename C >
+	template < typename C,
+		enable_if_t< (bool)compiles< C&, detail::has_end >() > = 0 >
 	constexpr auto end(C& c)noexcept(noexcept(c.end())){
 		return c.end();
 	}
 
-	template < typename C >
+	template < typename C,
+		enable_if_t< (bool)compiles< C const&, detail::has_end >() > = 0 >
 	constexpr auto end(C const& c)noexcept(noexcept(c.end())){
 		return c.end();
 	}
@@ -61,7 +74,34 @@ namespace mitrax{
 		return make_move_iterator(array + N);
 	}
 
+
+}
+
+
+namespace mitrax::detail{
+
+
 	template < typename C >
+	using support_begin = decltype(mitrax::begin(std::declval< C >()));
+
+	template < typename C >
+	using support_end = decltype(mitrax::begin(std::declval< C >()));
+
+
+}
+
+
+namespace mitrax{
+
+
+	template < typename C,
+		enable_if_t< (bool)compiles< C const&, detail::support_begin >() > = 0 >
+	constexpr auto cbegin(C const& c)noexcept(noexcept(mitrax::begin(c))){
+		return mitrax::begin(c);
+	}
+
+	template < typename C,
+		enable_if_t< (bool)compiles< C const&, detail::support_end >() > = 0 >
 	constexpr auto cend(C const& c)noexcept(noexcept(mitrax::end(c))){
 		return mitrax::end(c);
 	}
