b1ff43cc97dcd3b072ea19ff3d019a3265334224
Benjamin Buch <benni.buch@gmail.com>
2016-10-19 18:08:21 +0200

    remove GIL benchmark

--- benchmark/script/eval_sobel.sh
+++ benchmark/script/eval_sobel.sh
@@ -10,9 +10,6 @@ echo "Convert sobel_uBLAS.json"
 lua script/sobel_json2gnuplot.lua sobel_uBLAS.json > sobel_uBLAS.dat
 echo "Convert sobel_mitrax.json"
 lua script/sobel_json2gnuplot.lua sobel_mitrax.json > sobel_mitrax.dat
-# doesn't work yet
-# echo "Convert sobel_GIL.json"
-# lua script/sobel_json2gnuplot.lua sobel_GIL.json > sobel_GIL.dat
 
 echo "Plot script/sobel.plt"
 gnuplot script/sobel.plt
--- benchmark/script/run_sobel.sh
+++ benchmark/script/run_sobel.sh
@@ -17,5 +17,3 @@ echo "run sobel.uBLAS benchmark"
 bin/clang-*/release/sobel_uBLAS --benchmark_repetitions=$repetitions --benchmark_format=json > sobel_uBLAS.json
 echo "run sobel.mitrax benchmark"
 bin/clang-*/release/sobel_mitrax --benchmark_repetitions=$repetitions --benchmark_format=json > sobel_mitrax.json
-# doesn't work yet
-# bin/clang-*/release/sobel_GIL --benchmark_repetitions=$repetitions --benchmark_format=json > sobel_GIL.json
